2299bff536e6697969732fa24533c71cb99328bc
rohithpeddi7 <peddi.rohith.cse21@itbhu.ac.in>
2025-04-10 16:44:56 +0530

    Added more config

--- queue-cluster/queue_build.sh
+++ queue-cluster/queue_build.sh
@@ -1,8 +1,8 @@
-sudo docker-compose up -d
+sudo docker-compose up --build
 
-sudo docker exec -it kafka1 kafka-topics \
-  --bootstrap-server localhost:29092 \
+docker exec -it kafka1 kafka-topics \
   --create \
-  --topic jobs \
-  --partitions 3 \
-  --replication-factor 3
+  --topic jobs-topic \
+  --bootstrap-server localhost:29192 \
+  --replication-factor 2 \
+  --partitions 3
